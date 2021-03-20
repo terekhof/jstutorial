380f375fcd544220cbe76ce7088577c13739bdfb
terekhof <glebterekhof@gmail.com>
2021-03-20 20:50:34 +0400

    learn about bind(), DOM and so on.

---
+++ DomAssignment/assignment.js
@@ -0,0 +1,17 @@
+const firstElement = document.getElementById('task-1');
+const liElement = document.querySelector('li');
+
+firstElement.style.backgroundColor = 'black';
+liElement.style.color = 'white';
+
+
+
+const firstTitle = document.querySelector('title');
+
+firstTitle.innerText = 'Assignment - Solved!';
+
+
+
+const h1 = document.querySelector('h1');
+
+h1.innerText = "Assignment - Solved!";
--- Homework_4/assignment.js
+++ Homework_4/assignment.js
@@ -42,7 +42,7 @@ sayHello4("Gleb");
 Great
 */
 
-
+/*
 function checkInput(cb, ...strings) {
   let hasEmptyText = false;
   for (const text of strings) {
@@ -63,6 +63,30 @@ checkInput(
   'Hello',
   '232',
   'adada'
-);
+); */
 
 
+const combine = (resultHandler, operation, ...numbers) => {
+  const validateNumber = number => {
+    return isNaN(number) ? 0 : number;
+  };
+
+  let sum = 0;
+  for (const num of numbers) {
+    if (operation === 'ADD') {
+      sum += validateNumber(num);
+    } else if (operation === 'SUBTRACT') {
+      sum -= validateNumber(num);
+    }
+
+  }
+  resultHandler(sum, 'The result after adding all numbers is');
+}
+
+const showResult = (messageText, result) => {
+  alert(messageText + ' ' + result);
+}
+
+combine(showResult.bind(this, 'The result after adding all  numbers is:'), 'ADD', 1,10,15,20);
+combine(showResult.bind(this, 'The result after adding all  numbers is:'), 'ADD', 2,50,75,110);
+combine(showResult.bind(this, 'The result after subtracting all  numbers is:'), 'SUBTRACT', 16,1055,415,24);
