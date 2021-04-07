00d179bd73a533f066038cabf962d354a956d472
terekhof <glebterekhof@gmail.com>
2021-04-07 21:20:36 +0400

    review find, reduce, map, forEach, join, split and so on

--- Arrays/arrays-01-starting-code/app.js
+++ Arrays/arrays-01-starting-code/app.js
@@ -53,7 +53,7 @@ console.log(hobbies);
 const removedElements = hobbies.splice(-2, 1);
 console.log(removedElements); */
 
-const testResults = [1, 5.3, 1.5, 10.99, -5, 10];
+/* const testResults = [1, 5.3, 1.5, 10.99, -5, 10];
 //const storedResults = testResults.slice(2);
 const storedResults = testResults.concat([3.99, 2]);
 
@@ -64,3 +64,76 @@ console.log(testResults.indexOf(5.3));
 
 const personData = [{ name: "Max" }, { name: "Manuel"}];
 console.log(personData.indexOf({ name: "Manuel"}));
+
+const manuel = personData.find(person => person.name  === 'Manuel');
+
+manuel.name = 'Anna';
+
+console.log(manuel, personData);
+
+const maxIndex = personData.findIndex(person => person.name  === 'Max');
+
+console.log(maxIndex); */
+
+/* const prices = [10.99, 5.99, 3.99, 6.59];
+const tax = 0.19;
+const taxAdjustedPrices = [];
+
+/* for (const price of prices) {
+  taxAdjustedPrices.push(price * (1 + tax));
+}
+ */
+
+/* prices.forEach((price, idx, prices) => {
+  const priceObj = { index: idx, taxAdjPrice: price * (1 + tax) };
+  taxAdjustedPrices.push(priceObj);
+})
+
+console.log(taxAdjustedPrices) */
+
+/* const prices = [10.99, 5.99, 3.99, 6.59];
+const tax = 0.19;
+
+const taxAdjustedPrices = prices.map((price, idx, prices) => {
+  const priceObj = { index: idx, taxAdjPrice: price * (1 + tax) };
+  return priceObj;
+})
+
+/* console.log(prices, taxAdjustedPrices); */
+
+/* const sortedPrices = prices.sort((a, b) => {
+  if (a > b) {
+    return 1;
+  } else if (a === b) {
+    return 0;
+  } else {
+    return -1;
+  }
+});
+console.log(sortedPrices);
+
+const filteredArray = prices.filter(price => price > 6);
+
+console.log(filteredArray);
+
+const sum = prices.reduce((prevValue, curValue) => prevValue + curValue, 0);
+
+console.log(sum); */
+
+/* const data = 'newyork;10.88;2000';
+
+const transformedData = data.split(';');
+transformedData[1] = +transformedData[1];
+console.log(transformedData);
+
+const nameFragements = ['Max', 'Schwarz'];
+const name = nameFragements.join(' ');
+
+console.log(name); */
+
+const str = 'Reverse';
+
+const transformedString = str.split('').reverse().join('');
+
+console.log(transformedString);
+
