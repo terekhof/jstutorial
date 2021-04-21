6b5b4f34b6d08618d03a0a68ce867d459668bbb4
terekhof <glebterekhof@gmail.com>
2021-04-21 22:59:04 +0400

    Done with OOP and Classes

--- oop-assignment-problem/assignment.js
+++ oop-assignment-problem/assignment.js
@@ -1,4 +1,17 @@
 class Course {
+  #price;
+  
+  get price() {
+    return '$' + this.#price;
+  }
+
+  set price(value) {
+    if (value < 0) {
+      throw 'Invalid value!';
+    }
+    this.#price = value;
+  }
+
   constructor(title, length, price) {
     this.title = title;
     this.length = length;
@@ -6,49 +19,49 @@ class Course {
   }
 
   valueCalc() {
-    const valueCalcResult = this.length / this.price;
+    const valueCalcResult = this.length / this.#price;
     return valueCalcResult;
   }
 
   summaryPrint() {
-    const summary = `title: ${this.title} length: ${this.length} price: ${this.price}`;
-    return summary;
-  }
+    console.log(`title: ${this.title} length: ${this.length} price: ${this.price}`);
+  } 
 }
 
 firstCourse = new Course('First course', '48', 19.99);
 secondCourse = new Course('Second course', '96', 29.99);
 
-firstCourse.valueCalc();
-secondCourse.valueCalc();
 
 console.log(firstCourse, secondCourse);
-console.log(firstCourse.valueCalc(), secondCourse.valueCalc());
-console.log(firstCourse.summaryPrint(), secondCourse.summaryPrint());
+
+console.log(firstCourse.valueCalc());
+console.log(secondCourse.valueCalc());
+
+firstCourse.summaryPrint();
+secondCourse.summaryPrint(); 
 
 
 
 class PracticalCourse extends Course {
-  numOfExercises = 56;
+  constructor(title, length, price, numOfExercises) {
+    super(title, length, price);
+    this.numOfExercises = numOfExercises;
+  }
 }
+
+let practicalCourse =  new PracticalCourse('Practical Course', 16, 9.99, 20);
+
+console.log(practicalCourse)
+practicalCourse.summaryPrint();
+
+
 
 class TheoreticalCourse extends Course {
   publish() {
     console.log('Print something to the console');
-  }
-  constructor(title, length, price) {
-    super();
-    this.title = title;
-    this.length = length;
-    this.price = price;
-    this.publish();
-  }
-  
+  }  
 }
 
-let practicalCourse =  new PracticalCourse('Practical Course', '16', 9.99);
-let theoreticalCourse = new TheoreticalCourse('TheoreticalCourse', '32', 18.99);
-
-console.log(practicalCourse, theoreticalCourse);
-console.log(practicalCourse.valueCalc(), practicalCourse.summaryPrint());
-console.log(theoreticalCourse.valueCalc(), theoreticalCourse.summaryPrint());
+let theoreticalCourse = new TheoreticalCourse('TheoreticalCourse', 32, 18.99);
+theoreticalCourse.publish();
+theoreticalCourse.summaryPrint();
