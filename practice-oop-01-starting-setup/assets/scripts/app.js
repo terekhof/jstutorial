28eb4ad22e08c4810568d30822b1949281741e31
terekhof <glebterekhof@gmail.com>
2021-04-26 21:16:26 +0400

    Done with prototypes & OOP Practice

---
+++ practice-oop-01-starting-setup/assets/scripts/app.js
@@ -0,0 +1,44 @@
+class Tooltip {}
+
+class ProjectItem {
+  constructor (id) {
+    this.id = id;
+    this.connectMoreInfoButton();
+    this.connectSwitchButton();
+  }
+
+  connectMoreInfoButton () {
+
+  }
+
+  connectSwitchButton () {
+    const projectItemElement = document.getElementById(this.id);
+    const switchBtn = projectItemElement.querySelector('button:last-of-type');
+    switchBtn.addEventListener('click', );
+  }
+}
+
+class ProjectList {
+  projects = [];
+
+  constructor(type) {
+    const prjItems = document.querySelectorAll(`#${type}-projects li`);
+    for (const prjItem of prjItems) {
+      this.projects.push(new ProjectItem(prjItem.id));
+    }
+    console.log(this.projects);
+  }
+
+  switchProject() {
+    
+  }
+}
+
+class App {
+  static init() {
+    const activeProjectList = new ProjectList('active');
+    const finishedProjectList = new ProjectList('finished');
+  }
+}
+
+App.init();
